79e11643e49874f3908631b144219ba8a01f0084
Hayley K <mrs.kornilenko@gmail.com>
2018-08-30 19:14:27 -0400

    week activities

--- Activities/Week 12-MySQL/08-playlistRead/print.js
+++ Activities/Week 12-MySQL/08-playlistRead/print.js
@@ -2,78 +2,99 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: 'root',
-
-  // Your password
   password: 'password',
-  database: 'playlist_DB'
+  database: 'musicPlaylist1_DB'
 });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   connection.query('CREATE DATABASE musicPlaylist5_DB', function(err, res) {
+//     if (err) throw err;
+//     console.log(`DB Created`);
+
+//     let sql =
+//       'CREATE TABLE playlist (id INTEGER AUTO_INCREMENT NOT NULL, artist VARCHAR(250), song VARCHAR (250), genre VARCHAR (250), PRIMARY KEY (id))';
+//     connection.query(sql, function(err, res) {
+//       if (err) throw err;
+//       console.log(`Table Created`);
+//     });
+//   });
+
+//   console.log('connected!');
+//   // createMusic();
+//   // deleteMusic();
+//   // readMusic();
+//   // connection.end();
+// });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log('Your connected as id ' + connection.threadId);
 
-  connection.query('CREATE DATABASE playlist_DB;', function(err, res) {
+  connection.query('CREATE DATABASE musicPlaylist9_DB', function(err, res) {
     if (err) throw err;
     console.log(`DB Created`);
 
-    let sql =
-      'CREATE TABLE playlist (id INT NOT NULL AUTO_INCREMENT, artist VARCHAR(45) NULL, song VARCHAR (45) NULL, genre VARCHAR (45) NULL, PRIMARY KEY (id));';
+    console.log('connected to id' + connection.threadId);
+    // createMusic();
+  });
 
-    connection.query(sql, function(err, res) {
-      if (err) throw err;
-      console.log(`Table Created`);
-    });
-    // insertSong();
-    // updateQuery();
-    // afterConnection();
-    connection.end();
+  var sql =
+    'CREATE TABLE playlist (id INT NOT NULL AUTO_INCREMENT, artist VARCHAR(250), song VARCHAR(250), genre VARCHAR(250), PRIMARY KEY (id))';
+
+  connection.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log('Table created');
   });
 });
 
-// //insert with an object
-// function insertSong() {
-//   connection.query(
-//     'INSERT INTO playlist SET ?',
-//     {
-//       artist: 'Backstreet Boys',
-//       song: 'I want it that way',
-//       genre: 'Pop'
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res);
-//     }
-//   );
-// }
+function createMusic() {
+  console.log(`Creating a new row of music`);
 
-// //how to dynmanically update the db table - use an array with two objects
-// function updateQuery() {
-//   connection.query(
-//     'UPDATE playlist SET ? WHERE ?',
-//     [
-//       {
-//         flavor: 'cookies and cream'
-//       },
-//       {
-//         id: 1
-//       }
-//     ],
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res);
-//     }
-//   );
-// }
+  connection.query(
+    'INSERT INTO music (artis, song, genre) VALUES ?',
+    [
+      [
+        ['backstreet boys', 'I want it that way', 'pop'],
+        ['taylor swift', 'shake it off', 'pop']
+      ]
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log('Worked', res);
+    }
+  );
+}
 
-// function afterConnection() {
-//   connection.query('SELECT * FROM playlist', function(err, res) {
-//     if (err) throw err;
-//     console.log(res);
-//   });
-// };
+function readMusic() {
+  connection.query('SELECT * FROM music', function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+}
+
+function updateMusic() {
+  connection.query(
+    'UPDATE music SET ? WHERE ?',
+    [
+      {
+        genre: 'Pop'
+      },
+      {
+        id: 2
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log(res);
+    }
+  );
+}
+
+function deleteMusic() {
+  connection.query('DELETE FROM music WHERE id=?', [2], function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+}
