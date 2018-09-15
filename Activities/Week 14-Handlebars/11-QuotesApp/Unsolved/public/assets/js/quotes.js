25a5315482b4d92fdba3264c3bfe730cbba3656e
Hayley K <mrs.kornilenko@gmail.com>
2018-09-15 08:24:23 -0700

    updates

--- Activities/Week 14-Handlebars/11-QuotesApp/Unsolved/public/assets/js/quotes.js
+++ Activities/Week 14-Handlebars/11-QuotesApp/Unsolved/public/assets/js/quotes.js
@@ -1,6 +1,26 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
 
-  // jQuery event handlers should go here.
+  $(.create-form).submit(function (event) {
+    event.preventDefault();
+
+    let newQuote = {
+      author: $("#auth").val().trim(),
+      quote: $("#quo").val().trim()
+    }
+
+    // Send the POST request.
+    $.ajax("/api/quotes", {
+      type: "POST",
+      data: newQuote
+    }).then(
+      function() {
+        console.log("created new quote");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  
+  })
 
 });
